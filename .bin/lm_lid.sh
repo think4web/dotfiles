31e15d4f1b29b10e27dbf49c690d6349b7fa3d87
think4web <cheverdak.nikolay@gmail.com>
2020-06-06 12:44:26 +0300

    .

--- .bin/lm_lid.sh
+++ .bin/lm_lid.sh
@@ -1,17 +1,6 @@
 
 #!/bin/bash
 
-#set variables
-
-#Check lid state (return 0 if closed)
-check_lid () {
- grep -q closed /proc/acpi/button/lid/*/state
-}
-
-#Lock screen without sleep
-check_lid
-if [ $? = 0 ]
-then
-        DISPLAY=:0.0 su faust -c /home/faust/.local/bin/lock
+        /home/faust/.local/bin/lock
         systemctl suspend
-fi
+
