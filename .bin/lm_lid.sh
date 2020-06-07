d4d6c8e71fe264087b607837e4f567c3a0d895ea
think4web <cheverdak.nikolay@gmail.com>
2020-06-07 12:59:27 +0300

    .

--- .bin/lm_lid.sh
+++ .bin/lm_lid.sh
@@ -4,9 +4,8 @@ grep -q closed /proc/acpi/button/lid/*/state
 if [ $? = 0 ]; then
  lid_closed=1
  echo "Lid Closed"
- DISPLAY=:0.0 'sh /home/faust/.local/bin/lock'
- #DISPLAY=:0.0 su faust -c 'sh /home/faust/.local/bin/lock'
- sleep 1
+ DISPLAY=:0.0 su faust -c 'sh /home/faust/.local/bin/lock' &
+ sleep 1 &&
  systemctl  suspend
 else
  lid_closed=0
